c8bf5180c2ffde1a12be8204cc3b10b70e713e81
SwetiDevOps <sweti1855@gmail.com>
2025-08-16 19:52:07 -0500

    Update variable.tf

--- Assigments-1 /Modules/S3/variable.tf
+++ Assigments-1 /Modules/S3/variable.tf
@@ -1,1 +1,13 @@
 
+variable "bucket_name" { 
+    type = string 
+    }
+variable "lifecycle_days1" { 
+    type = number 
+    }
+variable "lifecycle_days2" {
+     type = number 
+     }
+variable "expiration_days" { 
+    type = number 
+    }
